03bc5db84282643f6908068487061121c6d838e3
Mir Muhammad Abidul Haq (Ahnaf) <76505613+Ahnaf-nub@users.noreply.github.com>
2024-08-05 17:57:46 +0600

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,17 @@
+document.getElementById('summarize-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const formData = new FormData(this);
+    fetch('http://127.0.0.1:5000/summarize', {
+      method: 'POST',
+      body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById('summary').innerText = data.summary;
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      document.getElementById('summary').innerText = 'Error summarizing text.';
+    });
+  });
+  
--- server/app.py
+++ server/app.py
@@ -1,28 +1,30 @@
-from groq import Groq
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+import requests
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+api_key = os.getenv('API_KEY')
 app = Flask(__name__)
+CORS(app)
 
-client = Groq(
-    api_key='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',
-)
+API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
+headers = {"Authorization": f"Bearer {api_key}"}
+
+def query(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
 
 @app.route('/summarize', methods=['POST'])
 def summarize():
     text = request.form['text']
-    chat_completion = client.chat.completions.create(
-        messages=[
-            {
-                "role": "user",
-                "content": f"Summarize the following text: {text}"
-            }
-        ],
-        model="llama3-70b-8192",
-    )
-    summary = chat_completion.choices[0].message.content
+    if len(text) > 100:
+        output = query({"inputs": text})
+        summary = output[0]['summary_text'] if 'summary_text' in output[0] else "Error: Unable to summarize text"
+    else:
+        summary = "The text is too short to be summarized"
     return jsonify({'summary': summary})
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
